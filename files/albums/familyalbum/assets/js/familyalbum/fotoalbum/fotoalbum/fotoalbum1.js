0cfe29e92a461ce394460163d9dbb9508971e52a
12Andrey76 <110424843+12Andrey76@users.noreply.github.com>
2024-12-30 17:16:24 +0400

    Create fotoalbum1.js

---
+++ files/albums/familyalbum/assets/js/familyalbum/fotoalbum/fotoalbum/fotoalbum1.js
@@ -0,0 +1,201 @@
+const photoBlocks = [
+  {
+    id: 'prev1',
+    title: 'Фотоблок #1',
+    images: [
+      '../foto/foto1.html#slide1',
+      '../foto/foto1.html#slide2',
+      '../foto/foto1.html#slide3',
+      '../foto/foto1.html#slide4',
+      '../foto/foto1.html#slide5',
+      '../foto/foto1.html#slide6',
+      '../foto/foto1.html#slide7',
+      '../foto/foto1.html#slide8',
+      '../foto/foto1.html#slide9',
+      '../foto/foto1.html#slide10',
+      '../foto/foto1.html#slide11',
+      '../foto/foto1.html#slide12',
+    ],
+    imagesSrc: [
+      'https://i8.wampi.ru/2024/01/13/1390d3e1c8c3f65c9.th.jpg', 'https://im.wampi.ru/2024/01/13/2afb769e6a38d81b3.th.jpg', 'https://i1.wampi.ru/2024/01/13/3ed111205620bf9de.th.jpg', 'https://i8.wampi.ru/2024/01/13/4f72d9e0e707b4c4e.th.jpg', 'https://im.wampi.ru/2024/01/13/56de1ebc9da3ae0a6.th.jpg', 'https://i1.wampi.ru/2024/01/13/6bca10b972944a583.th.jpg', 'https://i8.wampi.ru/2024/01/13/7a611a8eaf0b9c867.th.jpg', 'https://im.wampi.ru/2024/01/13/843e33a6cdaa6932f.th.jpg', 'https://i1.wampi.ru/2024/01/13/9d8371a1b7af65ff2.th.jpg', 'https://i8.wampi.ru/2024/01/13/10b2d281d1a084dbdc.th.jpg', 'https://im.wampi.ru/2024/01/13/112a4891c06634db7e.th.jpg', 'https://i1.wampi.ru/2024/01/13/1287706b33dbf25f80.th.jpg',
+    ]
+    
+  },
+
+  {
+    id: 'prev2',
+    title: 'Фотоблок #2',
+    images: [
+      '../foto/foto1.html#slide13',
+      '../foto/foto1.html#slide14',
+      '../foto/foto1.html#slide15',
+      '../foto/foto1.html#slide16',
+      '../foto/foto1.html#slide17',
+      '../foto/foto1.html#slide18',
+      '../foto/foto1.html#slide19',
+      '../foto/foto1.html#slide20',
+      '../foto/foto1.html#slide21',
+      '../foto/foto1.html#slide22',
+      '../foto/foto1.html#slide23',
+      '../foto/foto1.html#slide24',
+    ],
+    imagesSrc: [
+      'https://i8.wampi.ru/2024/01/13/13.th.jpg',
+      'https://im.wampi.ru/2024/01/13/14.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/15.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/16.th.jpg',
+      'https://im.wampi.ru/2024/01/13/17.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/18.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/19.th.jpg',
+      'https://im.wampi.ru/2024/01/13/20.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/21.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/22.th.jpg',
+      'https://im.wampi.ru/2024/01/13/23.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/24.th.jpg',
+    ]
+    
+  },
+
+  {
+    id: 'prev3',
+    title: 'Фотоблок #3',
+    images: [
+      '../foto/foto1.html#slide25',
+      '../foto/foto1.html#slide26',
+      '../foto/foto1.html#slide27',
+      '../foto/foto1.html#slide28',
+      '../foto/foto1.html#slide29',
+      '../foto/foto1.html#slide30',
+      '../foto/foto1.html#slide31',
+      '../foto/foto1.html#slide32',
+      '../foto/foto1.html#slide33',
+      '../foto/foto1.html#slide34',
+      '../foto/foto1.html#slide35',
+      '../foto/foto1.html#slide36',
+    ],
+    imagesSrc: [
+      'https://im.wampi.ru/2024/01/13/2521521ea576cfb0c0.th.jpg', 'https://i1.wampi.ru/2024/01/13/26d50d044a8454403d.th.jpg', 'https://i8.wampi.ru/2024/01/13/273a1d4e39287ce9b2.th.jpg', 'https://im.wampi.ru/2024/01/13/283067be76ae77cccf.th.jpg', 'https://i1.wampi.ru/2024/01/13/29a1b7811c6b31afc9.th.jpg', 'https://i8.wampi.ru/2024/01/13/30d193ed00658b9b82.th.jpg', 'https://im.wampi.ru/2024/01/13/314913eec250befbea.th.jpg', 'https://i1.wampi.ru/2024/01/13/32cb9944d2071a13e4.th.jpg', 'https://i8.wampi.ru/2024/01/13/3379bb95ea6e0b2d30.th.jpg', 'https://im.wampi.ru/2024/01/13/34b2c75359038ccbe2.th.jpg', 'https://i1.wampi.ru/2024/01/13/35bb8731c571c21885.th.jpg', 'https://i8.wampi.ru/2024/01/13/36943beac6c7156126.th.jpg',
+    ]
+    
+  },
+
+  {
+    id: 'prev4',
+    title: 'Фотоблок #4',
+    images: [
+      '../foto/foto1.html#slide37',
+      '../foto/foto1.html#slide38',
+      '../foto/foto1.html#slide39',
+      '../foto/foto1.html#slide40',
+      '../foto/foto1.html#slide41',
+      '../foto/foto1.html#slide42',
+      '../foto/foto1.html#slide43',
+      '../foto/foto1.html#slide44',
+      '../foto/foto1.html#slide45',
+      '../foto/foto1.html#slide46',
+      '../foto/foto1.html#slide47',
+      '../foto/foto1.html#slide48',
+    ],
+    imagesSrc: [
+      'https://im.wampi.ru/2024/01/13/37f3fa7cf32c04d8a8.th.jpg', 'https://i1.wampi.ru/2024/01/13/38e49dbebf9b082869.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/39.th.jpg',
+      'https://im.wampi.ru/2024/01/13/40.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/41.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/42.th.jpg',
+      'https://im.wampi.ru/2024/01/13/43.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/44.th.jpg', 'https://i8.wampi.ru/2024/01/13/45c557d1183911ba1c.th.jpg',
+      'https://im.wampi.ru/2024/01/13/46.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/47.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/48.th.jpg',
+    ]
+    
+  },
+
+  {
+    id: 'prev5',
+    title: 'Фотоблок #5',
+    images: [
+      '../foto/foto1.html#slide49',
+      '../foto/foto1.html#slide50',
+      '../foto/foto1.html#slide51',
+      '../foto/foto1.html#slide52',
+      '../foto/foto1.html#slide53',
+      '../foto/foto1.html#slide54',
+      '../foto/foto1.html#slide55',
+      '../foto/foto1.html#slide56',
+      '../foto/foto1.html#slide57',
+      '../foto/foto1.html#slide58',
+      '../foto/foto1.html#slide59',
+      '../foto/foto1.html#slide60',
+    ],
+    imagesSrc: [
+      'https://i1.wampi.ru/2024/01/13/49.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/50.th.jpg',
+      'https://im.wampi.ru/2024/01/13/51.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/52.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/53.th.jpg',
+      'https://im.wampi.ru/2024/01/13/54.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/55.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/56.th.jpg',
+      'https://im.wampi.ru/2024/01/13/57.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/58.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/59.th.jpg',
+      'https://im.wampi.ru/2024/01/13/60.th.jpg',
+    ]
+    
+  },
+
+  {
+    id: 'prev6',
+    title: 'Фотоблок #6',
+    images: [
+      '../foto/foto1.html#slide61',
+      '../foto/foto1.html#slide62',
+      '../foto/foto1.html#slide63',
+      '../foto/foto1.html#slide64',
+      '../foto/foto1.html#slide65',
+      '../foto/foto1.html#slide66',
+      '../foto/foto1.html#slide67',
+      '../foto/foto1.html#slide68',
+      '../foto/foto1.html#slide69',
+      '../foto/foto1.html#slide70',
+      '../foto/foto1.html#slide71',
+      '../foto/foto1.html#slide72',
+    ],
+    imagesSrc: [
+      'https://im.wampi.ru/2024/01/13/61.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/62.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/63.th.jpg', 'https://im.wampi.ru/2024/01/13/64a312cf112052264c.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/65.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/66.th.jpg',
+      'https://im.wampi.ru/2024/01/13/67.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/68.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/69.th.jpg',
+      'https://im.wampi.ru/2024/01/13/70.th.jpg',
+      'https://i1.wampi.ru/2024/01/13/71.th.jpg',
+      'https://i8.wampi.ru/2024/01/13/72.th.jpg',
+    ]
+    
+  }
+
+  // Добавьте больше фотоблоков по аналогии
+];
+
+function loadScript(url) {
+	return new Promise((resolve, reject) => {
+			const script = document.createElement('script');
+			script.src = url;
+			script.onload = () => resolve();
+			script.onerror = () => reject(new Error(`Failed to load script: ${url}`));
+			document.head.appendChild(script);
+	});
+}
+
+// Загрузка скрипта slides.js
+loadScript('../../assets/js/familyalbum/js-component/prev-slides.js')
+	.then(() => {
+			console.log('Slides script loaded successfully');
+	})
+	.catch(error => {
+			console.error(error);
+	});
