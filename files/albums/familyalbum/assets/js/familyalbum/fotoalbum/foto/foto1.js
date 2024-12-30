b5898564793cec1c02b1fe6c5742d501c290f7fa
12Andrey76 <110424843+12Andrey76@users.noreply.github.com>
2024-12-30 17:21:36 +0400

    Create foto1.js

---
+++ files/albums/familyalbum/assets/js/familyalbum/fotoalbum/foto/foto1.js
@@ -0,0 +1,957 @@
+const slides = [
+	{
+		id: 1,
+		image: "https://i8.wampi.ru/2024/01/13/1390d3e1c8c3f65c9.jpg",
+		title: "Фото 1",
+		block: 1,
+		links: {
+			previous: 12,
+			next: 2,
+			block: "Блок 1",
+			blockPrevious: 61,
+			blockNext: 13,
+	}
+	},
+	{
+		id: 2,
+		image: "https://im.wampi.ru/2024/01/13/2afb769e6a38d81b3.jpg",
+		title: "Фото 2",
+		block: 1,
+		links: {
+			previous: 1,
+			next: 3,
+			block: "Блок 1",
+			blockPrevious: 61,
+			blockNext: 13,
+		}
+	},
+	{
+		id: 3,
+		image: "https://i1.wampi.ru/2024/01/13/3ed111205620bf9de.jpg",
+		title: "Фото 3",
+		block: 1,
+		links: {
+			previous: 2,
+			next: 4,
+			block: "Блок 1",
+			blockPrevious: 61,
+			blockNext: 13,
+		}
+	},
+	{
+		id: 4,
+		image: "https://i8.wampi.ru/2024/01/13/4f72d9e0e707b4c4e.jpg",
+		title: "Фото 4",
+		block: 1,
+		links: {
+			previous: 3,
+			next: 5,
+			block: "Блок 1",
+			blockPrevious: 61,
+			blockNext: 13,
+		}
+	},
+	{
+		id: 5,
+		image: "https://im.wampi.ru/2024/01/13/56de1ebc9da3ae0a6.jpg",
+		title: "Фото 5",
+		block: 1,
+		links: {
+			previous: 4,
+			next: 6,
+			block: "Блок 1",
+			blockPrevious: 61,
+			blockNext: 13,
+	}
+	},
+	{
+		id: 6,
+		image: "https://i1.wampi.ru/2024/01/13/6bca10b972944a583.jpg",
+		title: "Фото 6",
+		block: 6,
+		links: {
+			previous: 5,
+			next: 7,
+			block: "Блок 1",
+			blockPrevious: 61,
+			blockNext: 13,
+		}
+	},
+	{
+		id: 7,
+		image: "https://i8.wampi.ru/2024/01/13/7a611a8eaf0b9c867.jpg",
+		title: "Фото 7",
+		block: 1,
+		links: {
+			previous: 6,
+			next: 8,
+			block: "Блок 1",
+			blockPrevious: 61,
+			blockNext: 13,
+		}
+	},
+	{
+		id: 8,
+		image: "https://im.wampi.ru/2024/01/13/843e33a6cdaa6932f.jpg",
+		title: "Фото 8",
+		block: 1,
+		links: {
+			previous: 7,
+			next: 9,
+			block: "Блок 1",
+			blockPrevious: 61,
+			blockNext: 13,
+		}
+	},
+	{
+		id: 9,
+		image: "https://i1.wampi.ru/2024/01/13/9d8371a1b7af65ff2.jpg",
+		title: "Фото 9",
+		block: 1,
+		links: {
+			previous: 8,
+			next: 10,
+			block: "Блок 1",
+			blockPrevious: 61,
+			blockNext: 13,
+		}
+	},
+	{
+		id: 10,
+		image: "https://i8.wampi.ru/2024/01/13/10b2d281d1a084dbdc.jpg",
+		title: "Фото 10",
+		block: 1,
+		links: {
+			previous: 9,
+			next: 11,
+			block: "Блок 1",
+			blockPrevious: 61,
+			blockNext: 13,
+		}
+	},
+	{
+		id: 11,
+		image: "https://im.wampi.ru/2024/01/13/112a4891c06634db7e.jpg",
+		title: "Фото 11",
+		block: 1,
+		links: {
+			previous: 10,
+			next: 12,
+			block: "Блок 1",
+			blockPrevious: 61,
+			blockNext: 13,
+		}
+	},
+	{
+		id: 12,
+		image: "https://i1.wampi.ru/2024/01/13/1287706b33dbf25f80.jpg",
+		title: "Фото 12",
+		block: 1,
+		links: {
+			previous: 11,
+			next: 1,
+			block: "Блок 1",
+			blockPrevious: 61,
+			blockNext: 13,
+		}
+	},
+	{
+		id: 13,
+		image: "https://i8.wampi.ru/2024/01/13/13.jpg",
+		title: "Фото 1",
+		block: 2,
+		links: {
+			previous: 24,
+			next: 14,
+			block: "Блок 2",
+			blockPrevious: 1,
+			blockNext: 25,
+		}
+	},
+	{
+		id: 14,
+		image: "https://im.wampi.ru/2024/01/13/14.jpg",
+		title: "Фото 2",
+		block: 2,
+		links: {
+			previous: 13,
+			next: 15,
+			block: "Блок 2",
+			blockPrevious: 1,
+			blockNext: 25,
+		}
+	},
+	{
+		id: 15,
+		image: "https://i1.wampi.ru/2024/01/13/15.jpg",
+		title: "Фото 3",
+		block: 2,
+		links: {
+			previous: 14,
+			next: 16,
+			block: "Блок 2",
+			blockPrevious: 1,
+			blockNext: 25,
+		}
+	},
+	{
+		id: 16,
+		image: "https://i8.wampi.ru/2024/01/13/16.jpg",
+		title: "Фото 4",
+		block: 2,
+		links: {
+			previous: 15,
+			next: 17,
+			block: "Блок 2",
+			blockPrevious: 1,
+			blockNext: 25,
+		}
+	},
+	{
+		id: 17,
+		image: "https://im.wampi.ru/2024/01/13/17.jpg",
+		title: "Фото 5",
+		block: 2,
+		links: {
+			previous: 16,
+			next: 18,
+			block: "Блок 2",
+			blockPrevious: 1,
+			blockNext: 25,
+		}
+	},
+	{
+		id: 18,
+		image: "https://i1.wampi.ru/2024/01/13/18.jpg",
+		title: "Фото 6",
+		block: 2,
+		links: {
+			previous: 17,
+			next: 19,
+			block: "Блок 2",
+			blockPrevious: 1,
+			blockNext: 25,
+		}
+	},
+	{
+		id: 19,
+		image: "https://i8.wampi.ru/2024/01/13/19.jpg",
+		title: "Фото 7",
+		block: 2,
+		links: {
+			previous: 18,
+			next: 20,
+			block: "Блок 2",
+			blockPrevious: 1,
+			blockNext: 25,
+		}
+	},
+	{
+		id: 20,
+		image: "https://im.wampi.ru/2024/01/13/20.jpg",
+		title: "Фото 8",
+		block: 2,
+		links: {
+			previous: 19,
+			next: 21,
+			block: "Блок 2",
+			blockPrevious: 1,
+			blockNext: 25,
+		}
+	},
+	{
+		id: 21,
+		image: "https://i1.wampi.ru/2024/01/13/21.jpg",
+		title: "Фото 9",
+		block: 2,
+		links: {
+			previous: 20,
+			next: 22,
+			block: "Блок 2",
+			blockPrevious: 1,
+			blockNext: 25,
+		}
+	},
+	{
+		id: 22,
+		image: "https://i8.wampi.ru/2024/01/13/22.jpg",
+		title: "Фото 10",
+		block: 2,
+		links: {
+			previous: 21,
+			next: 23,
+			block: "Блок 2",
+			blockPrevious: 1,
+			blockNext: 25,
+		}
+	},
+	{
+		id: 23,
+		image: "https://im.wampi.ru/2024/01/13/23.jpg",
+		title: "Фото 11",
+		block: 2,
+		links: {
+			previous: 22,
+			next: 24,
+			block: "Блок 2",
+			blockPrevious: 1,
+			blockNext: 25,
+		}
+	},
+	{
+		id: 24,
+		image: "https://i1.wampi.ru/2024/01/13/24.jpg",
+		title: "Фото 12",
+		block: 2,
+		links: {
+			previous: 23,
+			next: 13,
+			block: "Блок 2",
+			blockPrevious: 1,
+			blockNext: 25,
+		}
+	},
+	{
+		id: 25,
+		image: "https://im.wampi.ru/2024/01/13/2521521ea576cfb0c0.jpg",
+		title: "Фото 1",
+		block: 3,
+		links: {
+			previous: 36,
+			next: 26,
+			block: "Блок 3",
+			blockPrevious: 13,
+			blockNext: 37,
+		}
+	},
+	{
+		id: 26,
+		image: "https://i1.wampi.ru/2024/01/13/26d50d044a8454403d.jpg",
+		title: "Фото 2",
+		block: 3,
+		links: {
+			previous: 25,
+			next: 27,
+			block: "Блок 3",
+			blockPrevious: 13,
+			blockNext: 37,
+		}
+	},
+	{
+		id: 27,
+		image: "https://i8.wampi.ru/2024/01/13/273a1d4e39287ce9b2.jpg",
+		title: "Фото 3",
+		block: 3,
+		links: {
+			previous: 26,
+			next: 28,
+			block: "Блок 3",
+			blockPrevious: 13,
+			blockNext: 37,
+		}
+	},
+	{
+		id: 28,
+		image: "https://im.wampi.ru/2024/01/13/283067be76ae77cccf.jpg",
+		title: "Фото 4",
+		block: 3,
+		links: {
+			previous: 27,
+			next: 29,
+			block: "Блок 3",
+			blockPrevious: 13,
+			blockNext: 37,
+		}
+	},
+	{
+		id: 29,
+		image: "https://i1.wampi.ru/2024/01/13/29a1b7811c6b31afc9.jpg",
+		title: "Фото 5",
+		block: 3,
+		links: {
+			previous: 28,
+			next: 30,
+			block: "Блок 3",
+			blockPrevious: 13,
+			blockNext: 37,
+		}
+	},
+	{
+		id: 30,
+		image: "https://i8.wampi.ru/2024/01/13/30d193ed00658b9b82.jpg",
+		title: "Фото 6",
+		block: 3,
+		links: {
+			previous: 29,
+			next: 31,
+			block: "Блок 3",
+			blockPrevious: 13,
+			blockNext: 37,
+		}
+	},
+	{
+		id: 31,
+		image: "https://im.wampi.ru/2024/01/13/314913eec250befbea.jpg",
+		title: "Фото 7",
+		block: 3,
+		links: {
+			previous: 30,
+			next: 32,
+			block: "Блок 3",
+			blockPrevious: 13,
+			blockNext: 37,
+		}
+	},
+	{
+		id: 32,
+		image: "https://i1.wampi.ru/2024/01/13/32cb9944d2071a13e4.jpg",
+		title: "Фото 8",
+		block: 3,
+		links: {
+			previous: 31,
+			next: 33,
+			block: "Блок 3",
+			blockPrevious: 13,
+			blockNext: 37,
+		}
+	},
+	{
+		id: 33,
+		image: "https://i8.wampi.ru/2024/01/13/3379bb95ea6e0b2d30.jpg",
+		title: "Фото 9",
+		block: 3,
+		links: {
+			previous: 32,
+			next: 34,
+			block: "Блок 3",
+			blockPrevious: 13,
+			blockNext: 37,
+		}
+	},
+	{
+		id: 34,
+		image: "https://im.wampi.ru/2024/01/13/34b2c75359038ccbe2.jpg",
+		title: "Фото 10",
+		block: 3,
+		links: {
+			previous: 33,
+			next: 35,
+			block: "Блок 3",
+			blockPrevious: 13,
+			blockNext: 37,
+		}
+	},
+	{
+		id: 35,
+		image: "https://i1.wampi.ru/2024/01/13/35bb8731c571c21885.jpg",
+		title: "Фото 11",
+		block: 3,
+		links: {
+			previous: 34,
+			next: 36,
+			block: "Блок 3",
+			blockPrevious: 13,
+			blockNext: 37,
+		}
+	},
+	{
+		id: 36,
+		image: "https://i8.wampi.ru/2024/01/13/36943beac6c7156126.jpg",
+		title: "Фото 12",
+		block: 3,
+		links: {
+			previous: 35,
+			next: 25,
+			block: "Блок 3",
+			blockPrevious: 13,
+			blockNext: 37,
+		}
+	},
+	{
+		id: 37,
+		image: "https://im.wampi.ru/2024/01/13/37f3fa7cf32c04d8a8.jpg",
+		title: "Фото 1",
+		block: 4,
+		links: {
+			previous: 48,
+			next: 38,
+			block: "Блок 4",
+			blockPrevious: 25,
+			blockNext: 49,
+		}
+	},
+	{
+		id: 38,
+		image: "https://i1.wampi.ru/2024/01/13/38e49dbebf9b082869.jpg",
+		title: "Фото 2",
+		block: 4,
+		links: {
+			previous: 37,
+			next: 39,
+			block: "Блок 4",
+			blockPrevious: 25,
+			blockNext: 49,
+		}
+	},
+	{
+		id: 39,
+		image: "https://i8.wampi.ru/2024/01/13/39.jpg",
+		title: "Фото 3",
+		block: 4,
+		links: {
+			previous: 38,
+			next: 40,
+			block: "Блок 4",
+			blockPrevious: 25,
+			blockNext: 49,
+		}
+	},
+	{
+		id: 40,
+		image: "https://im.wampi.ru/2024/01/13/40.jpg",
+		title: "Фото 4",
+		block: 4,
+		links: {
+			previous: 39,
+			next: 41,
+			block: "Блок 4",
+			blockPrevious: 25,
+			blockNext: 49,
+		}
+	},
+	{
+		id: 41,
+		image: "https://i1.wampi.ru/2024/01/13/41.jpg",
+		title: "Фото 5",
+		block: 4,
+		links: {
+			previous: 40,
+			next: 42,
+			block: "Блок 4",
+			blockPrevious: 25,
+			blockNext: 49,
+		}
+	},
+	{
+		id: 42,
+		image: "https://i8.wampi.ru/2024/01/13/42.jpg",
+		title: "Фото 6",
+		block: 4,
+		links: {
+			previous: 41,
+			next: 43,
+			block: "Блок 4",
+			blockPrevious: 25,
+			blockNext: 49,
+		}
+	},
+	{
+		id: 43,
+		image: "https://im.wampi.ru/2024/01/13/43.jpg",
+		title: "Фото 7",
+		block: 4,
+		links: {
+			previous: 42,
+			next: 44,
+			block: "Блок 4",
+			blockPrevious: 25,
+			blockNext: 49,
+		}
+	},
+	{
+		id: 44,
+		image: "https://i1.wampi.ru/2024/01/13/44.jpg",
+		title: "Фото 8",
+		block: 4,
+		links: {
+			previous: 43,
+			next: 45,
+			block: "Блок 4",
+			blockPrevious: 25,
+			blockNext: 49,
+		}
+	},
+	{
+		id: 45,
+		image: "https://i8.wampi.ru/2024/01/13/45c557d1183911ba1c.jpg",
+		title: "Фото 9",
+		block: 4,
+		links: {
+			previous: 44,
+			next: 46,
+			block: "Блок 4",
+			blockPrevious: 25,
+			blockNext: 49,
+		}
+	},
+	{
+		id: 46,
+		image: "https://im.wampi.ru/2024/01/13/46.jpg",
+		title: "Фото 10",
+		block: 4,
+		links: {
+			previous: 45,
+			next: 47,
+			block: "Блок 4",
+			blockPrevious: 25,
+			blockNext: 49,
+		}
+	},
+	{
+		id: 47,
+		image: "https://i1.wampi.ru/2024/01/13/47.jpg",
+		title: "Фото 11",
+		block: 2,
+		links: {
+			previous: 46,
+			next: 48,
+			block: "Блок 4",
+			blockPrevious: 25,
+			blockNext: 49,
+		}
+	},
+	{
+		id: 48,
+		image: "https://i8.wampi.ru/2024/01/13/48.jpg",
+		title: "Фото 12",
+		block: 4,
+		links: {
+			previous: 47,
+			next: 37,
+			block: "Блок 4",
+			blockPrevious: 25,
+			blockNext: 49,
+		}
+	},
+	{
+		id: 49,
+		image: "https://i1.wampi.ru/2024/01/13/49.jpg",
+		title: "Фото 1",
+		block: 5,
+		links: {
+			previous: 60,
+			next: 50,
+			block: "Блок 5",
+			blockPrevious: 37,
+			blockNext: 61,
+		}
+	},
+	{
+		id: 50,
+		image: "https://i8.wampi.ru/2024/01/13/50.jpg",
+		title: "Фото 2",
+		block: 5,
+		links: {
+			previous: 49,
+			next: 51,
+			block: "Блок 5",
+			blockPrevious: 37,
+			blockNext: 61,
+		}
+	},
+	{
+		id: 51,
+		image: "https://im.wampi.ru/2024/01/13/51.jpg",
+		title: "Фото 3",
+		block: 5,
+		links: {
+			previous: 50,
+			next: 52,
+			block: "Блок 5",
+			blockPrevious: 37,
+			blockNext: 61,
+		}
+	},
+	{
+		id: 52,
+		image: "https://i1.wampi.ru/2024/01/13/52.jpg",
+		title: "Фото 4",
+		block: 5,
+		links: {
+			previous: 51,
+			next: 53,
+			block: "Блок 5",
+			blockPrevious: 37,
+			blockNext: 61,
+		}
+	},
+	{
+		id: 53,
+		image: "https://i8.wampi.ru/2024/01/13/53.jpg",
+		title: "Фото 5",
+		block: 5,
+		links: {
+			previous: 52,
+			next: 54,
+			block: "Блок 5",
+			blockPrevious: 37,
+			blockNext: 61,
+		}
+	},
+	{
+		id: 54,
+		image: "https://im.wampi.ru/2024/01/13/54.jpg",
+		title: "Фото 6",
+		block: 5,
+		links: {
+			previous: 53,
+			next: 55,
+			block: "Блок 5",
+			blockPrevious: 37,
+			blockNext: 61,
+		}
+	},
+	{
+		id: 55,
+		image: "https://i1.wampi.ru/2024/01/13/55.jpg",
+		title: "Фото 7",
+		block: 5,
+		links: {
+			previous: 54,
+			next: 56,
+			block: "Блок 5",
+			blockPrevious: 37,
+			blockNext: 61,
+		}
+	},
+	{
+		id: 56,
+		image: "https://i8.wampi.ru/2024/01/13/56.jpg",
+		title: "Фото 8",
+		block: 5,
+		links: {
+			previous: 55,
+			next: 57,
+			block: "Блок 5",
+			blockPrevious: 37,
+			blockNext: 61,
+		}
+	},
+	{
+		id: 57,
+		image: "https://im.wampi.ru/2024/01/13/57.jpg",
+		title: "Фото 9",
+		block: 5,
+		links: {
+			previous: 56,
+			next: 58,
+			block: "Блок 5",
+			blockPrevious: 37,
+			blockNext: 61,
+		}
+	},
+	{
+		id: 58,
+		image: "https://i1.wampi.ru/2024/01/13/58.jpg",
+		title: "Фото 10",
+		block: 5,
+		links: {
+			previous: 57,
+			next: 59,
+			block: "Блок 5",
+			blockPrevious: 37,
+			blockNext: 61,
+		}
+	},
+	{
+		id: 59,
+		image: "https://i8.wampi.ru/2024/01/13/59.jpg",
+		title: "Фото 11",
+		block: 5,
+		links: {
+			previous: 58,
+			next: 60,
+			block: "Блок 5",
+			blockPrevious: 37,
+			blockNext: 61,
+		}
+	},
+	{
+		id: 60,
+		image: "https://im.wampi.ru/2024/01/13/60.jpg",
+		title: "Фото 12",
+		block: 5,
+		links: {
+			previous: 59,
+			next: 49,
+			block: "Блок 5",
+			blockPrevious: 37,
+			blockNext: 61,
+		}
+	},
+	{
+		id: 61,
+		image: "https://im.wampi.ru/2024/01/13/61.jpg",
+		title: "Фото 1",
+		block: 6,
+		links: {
+			previous: 72,
+			next: 62,
+			block: "Блок 6",
+			blockPrevious: 49,
+			blockNext: 1,
+		}
+	},
+	{
+		id: 62,
+		image: "https://i1.wampi.ru/2024/01/13/62.jpg",
+		title: "Фото 2",
+		block: 6,
+		links: {
+			previous: 61,
+			next: 63,
+			block: "Блок 6",
+			blockPrevious: 49,
+			blockNext: 1,
+		}
+	},
+	{
+		id: 63,
+		image: "https://i8.wampi.ru/2024/01/13/63.jpg",
+		title: "Фото 3",
+		block: 6,
+		links: {
+			previous: 62,
+			next: 64,
+			block: "Блок 6",
+			blockPrevious: 49,
+			blockNext: 1,
+		}
+	},
+	{
+		id: 64,
+		image: "https://im.wampi.ru/2024/01/13/64a312cf112052264c.jpg",
+		title: "Фото 4",
+		block: 6,
+		links: {
+			previous: 63,
+			next: 65,
+			block: "Блок 6",
+			blockPrevious: 49,
+			blockNext: 1,
+		}
+	},
+	{
+		id: 65,
+		image: "https://i1.wampi.ru/2024/01/13/65.jpg",
+		title: "Фото 5",
+		block: 6,
+		links: {
+			previous: 64,
+			next: 66,
+			block: "Блок 6",
+			blockPrevious: 49,
+			blockNext: 1,
+		}
+	},
+	{
+		id: 66,
+		image: "https://i8.wampi.ru/2024/01/13/66.jpg",
+		title: "Фото 6",
+		block: 6,
+		links: {
+			previous: 65,
+			next: 67,
+			block: "Блок 6",
+			blockPrevious: 49,
+			blockNext: 1,
+		}
+	},
+	{
+		id: 67,
+		image: "https://im.wampi.ru/2024/01/13/67.jpg",
+		title: "Фото 7",
+		block: 6,
+		links: {
+			previous: 66,
+			next: 68,
+			block: "Блок 6",
+			blockPrevious: 49,
+			blockNext: 1,
+		}
+	},
+	{
+		id: 68,
+		image: "https://i1.wampi.ru/2024/01/13/68.jpg",
+		title: "Фото 8",
+		block: 6,
+		links: {
+			previous: 67,
+			next: 69,
+			block: "Блок 6",
+			blockPrevious: 49,
+			blockNext: 1,
+		}
+	},
+	{
+		id: 69,
+		image: "https://i8.wampi.ru/2024/01/13/69.jpg",
+		title: "Фото 9",
+		block: 6,
+		links: {
+			previous: 68,
+			next: 70,
+			block: "Блок 6",
+			blockPrevious: 49,
+			blockNext: 1,
+		}
+	},
+	{
+		id: 70,
+		image: "https://im.wampi.ru/2024/01/13/70.jpg",
+		title: "Фото 10",
+		block: 6,
+		links: {
+			previous: 69,
+			next: 71,
+			block: "Блок 6",
+			blockPrevious: 49,
+			blockNext: 1,
+		}
+	},
+	{
+		id: 71,
+		image: "https://i1.wampi.ru/2024/01/13/71.jpg",
+		title: "Фото 11",
+		block: 6,
+		links: {
+			previous: 70,
+			next: 72,
+			block: "Блок 6",
+			blockPrevious: 49,
+			blockNext: 1,
+		}
+	},
+	{
+		id: 72,
+		image: "https://i8.wampi.ru/2024/01/13/72.jpg",
+		title: "Фото 12",
+		block: 6,
+		links: {
+			previous: 71,
+			next: 61,
+			block: "Блок 6",
+			blockPrevious: 49,
+			blockNext: 1,
+		}
+	},
+];
+
+function loadScript(url) {
+	return new Promise((resolve, reject) => {
+			const script = document.createElement('script');
+			script.src = url;
+			script.onload = () => resolve();
+			script.onerror = () => reject(new Error(`Failed to load script: ${url}`));
+			document.head.appendChild(script);
+	});
+}
+
+// Загрузка скрипта slides.js
+loadScript('../../assets/js/familyalbum/js-component/foto-slides.js')
+	.then(() => {
+			console.log('Slides script loaded successfully');
+	})
+	.catch(error => {
+			console.error(error);
+	});
