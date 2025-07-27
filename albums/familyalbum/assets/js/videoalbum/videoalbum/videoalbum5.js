51625987e9fbe84de12201e33804c341e9c4d269
12Andrey76 <110424843+12Andrey76@users.noreply.github.com>
2025-07-27 19:43:50 +0400

    first commit
    first commit

--- albums/familyalbum/assets/js/videoalbum/videoalbum/videoalbum5.js
+++ albums/familyalbum/assets/js/videoalbum/videoalbum/videoalbum5.js
@@ -2,11 +2,11 @@ const albums = [
 	{
 		title: 'Блок #1',
 		images: [
-			{ src: 'https://s2.radikal.cloud/2025/04/22/184a1c9a07010497a.th.jpg', href: '../video/video5.html#slide1', alt: 'preview 1' },
-			{ src: 'https://s2.radikal.cloud/2025/04/22/2067ffd5c5808fe6b.th.jpg', href: '../video/video5.html#slide2', alt: 'preview 2' },
-			{ src: 'https://s2.radikal.cloud/2025/05/01/3af68c174640db867.th.jpg', href: '../video/video5.html#slide3', alt: 'preview 3' },
-			{ src: 'https://s1.radikal.cloud/2025/05/19/40d8defc7491f8a2d.th.jpg', href: '../video/video5.html#slide4', alt: 'preview 4' },
-			{ src: 'https://s3.radikal.cloud/2025/07/07/10000034106feb369f9d153d80.th.jpg', href: '../video/video5.html#slide5', alt: 'preview 5' },
+			{ src: 'https://radika1.link/2025/07/27/1000005900b6167ca4e2225f09.th.jpg', href: '../video/video5.html#slide1', alt: 'preview 1' },
+			{ src: 'https://radika1.link/2025/07/27/1000005901c88185c014d58a70.th.jpg', href: '../video/video5.html#slide2', alt: 'preview 2' },
+			{ src: 'https://radika1.link/2025/07/27/10000059026573c1a7e80c29d0.th.jpg', href: '../video/video5.html#slide3', alt: 'preview 3' },
+			{ src: 'https://radika1.link/2025/07/27/100000590307468f2b575fbcb6.th.jpg', href: '../video/video5.html#slide4', alt: 'preview 4' },
+			{ src: 'https://radika1.link/2025/07/27/1000005904f906737928cc8ecf.th.jpg', href: '../video/video5.html#slide5', alt: 'preview 5' },
 
 		],
 	},
