dd951482042e3efdd2917b0db9d512c84e1d9ae5
12Andrey76 <110424843+12Andrey76@users.noreply.github.com>
2026-04-29 13:52:25 +0400

    Update videoalbum6.js

--- albums/assets/js/familyalbum/videoalbum/videoalbum6.js
+++ albums/assets/js/familyalbum/videoalbum/videoalbum6.js
@@ -2,7 +2,7 @@ const carouselItems = [
   {
     title: "Блок #1",
     slides: [
-      { imgSrc: 'https://s2.radikal.cloud/2026/04/25/17bb79a43b613a686.th.jpg', link: 'video6/video1.html#slide1', alt: 'preview 1' },
+      { imgSrc: 'https://s1.radikal.cloud/2026/04/29/10000387038e7ca1cb1f7fc0d6.th.jpg', link: 'video6/video1.html#slide1', alt: 'preview 1' },
     ]
   },
 ];
